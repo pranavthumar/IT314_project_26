c93892882fad753ec373aa7e5564a0a0b69d089e
pranavthumar <86405881+pranavthumar@users.noreply.github.com>
2023-04-22 01:35:16 +0530

    Delete code/frontend/src/Instructor dashboard directory

--- code/frontend/src/Instructor dashboard/teacher-dashboard/src/context/ClassroomContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const ClassroomContext = React.createContext({
-  cohortData: {}
-})
-
-export default ClassroomContext
--- code/frontend/src/Instructor dashboard/teacher-dashboard/src/context/TimePeriodContext.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const TimePeriodContext = React.createContext()
-
-export default TimePeriodContext
